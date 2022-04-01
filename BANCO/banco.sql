d626ada03a30f7b93fe57d90fdcf6b3fd61dc6e3
Alexandre Costa <80131663+AlexandreCosta02@users.noreply.github.com>
2022-04-01 10:32:20 -0300

    Add files via upload

--- BANCO/banco.sql
+++ BANCO/banco.sql
@@ -1,1 +1,17 @@
+create database DB_BUSCA_CEP;
 
+use database DB_BUSCA_CEP;
+
+create table TB_BUSCA(
+    ID int not null AUTO_INCREMENT primary key,
+    CEP varchar(10) not null,
+    LOCALIDADE varchar(150),
+    LOGRADOURO varchar(250),
+    COMPLEMENTO varchar(150),
+    BAIRRO varchar(150),  
+    UF varchar(2),
+    DDD int,
+    IBGE int,
+    GIA int,
+    SIAFI int
+);
